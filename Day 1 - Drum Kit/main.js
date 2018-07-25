800fc1153bcabe1cd7ba4d17982048dff6b33b47
Travis <travishn@uci.edu>
2018-07-25 16:42:36 -0700

    Add event listener to key presses

--- Day 1 - Drum Kit/main.js
+++ Day 1 - Drum Kit/main.js
@@ -11,7 +11,10 @@ const keys = [
 ];
 
 const handleKeyPress = (e) => {
-
+  const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
 };
 
 const runApp = () => {
