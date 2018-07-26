fc5d7059c83dea5696032ff7c58aa96b840052a8
Travis <travishn@uci.edu>
2018-07-26 13:43:49 -0700

    Add initial tick function

---
+++ Day 2 - Clock/main.js
@@ -0,0 +1,11 @@
+const tick = () => {
+  const secondHand = document.querySelector('.second-hand');
+  const minuteHand = document.querySelector('.minute-hand');
+  const hourHand = document.querySelector('.hour-hand');
+
+
+  
+
+};
+
+window.setInterval(tick, 1000);
