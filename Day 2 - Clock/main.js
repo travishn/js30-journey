8ce75da20c7dc31618202cc30280885660365f40
Travis <travishn@uci.edu>
2018-07-26 17:00:04 -0700

    Fix header and clock hand positions

--- Day 2 - Clock/main.js
+++ Day 2 - Clock/main.js
@@ -1,7 +1,18 @@
 const tick = () => {
   const currentTime = new Date();
   const secondsDegree = (currentTime.getSeconds() / 60 * 360);
-  document.querySelector('.sec-hand').style.transform = `rotate(${secondsDegree}deg)`;
+  const minutesDegree = (currentTime.getMinutes() * 6) + (currentTime.getSeconds() / 60);
+  const hoursDegree = (currentTime.getHours() * 30) + (currentTime.getMinutes() / 60);
+
+  document.querySelector('.sec-hand').
+    style.transform = `rotate(${secondsDegree}deg)`;
+
+  document.querySelector('.min-hand').
+    style.transform = `rotate(${minutesDegree}deg)`;
+
+  document.querySelector('.hr-hand').
+    style.transform = `rotate(${hoursDegree}deg)`;
 };
+
 
 window.setInterval(tick, 1000);
